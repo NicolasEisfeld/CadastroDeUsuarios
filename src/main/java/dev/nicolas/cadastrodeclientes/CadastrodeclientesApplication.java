8d3f18d21fed2ca54f5b5ed5a5eb71bfddc708c8
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2025-09-16 21:36:17 -0300

    first commit

---
+++ src/main/java/dev/nicolas/cadastrodeclientes/CadastrodeclientesApplication.java
@@ -0,0 +1,13 @@
+package dev.nicolas.cadastrodeclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nicolas/cadastrodeclientes/CadastrodeclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicolas.cadastrodeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
