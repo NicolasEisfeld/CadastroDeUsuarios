35b7621e2d52c1cb5356c92d1dbb0ed1da5e5ad2
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2026-01-15 11:43:41 -0300

    feat: Mock dos Endpoints do Usuário

--- src/main/java/dev/nicolas/cadastrodeusuarios/Usuario/controller/UsuarioController.java
+++ src/main/java/dev/nicolas/cadastrodeusuarios/Usuario/controller/UsuarioController.java
@@ -1,8 +1,6 @@
 package dev.nicolas.cadastrodeusuarios.Usuario.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,6 +10,35 @@ public class UsuarioController {
         return "Olá mundo!";
     }
 
+    // Adicionar Usuário (create)
+    @PostMapping("/adicionar")
+    public String adicionarUsuario() {
+        return "Usuário adicionado";
+    }
+
+    // Procurar Usuario por ID (read)
+    @GetMapping("/procurar")
+    public String procurarUsuario() {
+        return "Usuário encontrado";
+    }
+
+    // Listar Todos os Usuários (read)
+    @GetMapping("/listartodos")
+    public String listarUsuarios() {
+        return "Lista de Usuários";
+    }
+
+    // Alterar Usuário (update)
+    @PutMapping("/alterar")
+    public String alterarUsuario() {
+        return "Usuario Alterado";
+    }
+
+    // Deletar Usuário (delete)
+    @DeleteMapping
+    public String removerUsuario() {
+        return "Usuario Removido";
+    }
 
 
 }
