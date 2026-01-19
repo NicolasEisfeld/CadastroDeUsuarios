9413d136e2c5158d45617d6ae12e7dd939d62203
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2026-01-19 09:32:31 -0300

    feat: cria a camada de serviços de Tarefa

---
+++ src/main/java/dev/nicolas/cadastrodeusuarios/Tarefas/service/TarefaService.java
@@ -0,0 +1,37 @@
+package dev.nicolas.cadastrodeusuarios.Tarefas.service;
+
+import dev.nicolas.cadastrodeusuarios.Tarefas.model.TarefaModel;
+import dev.nicolas.cadastrodeusuarios.Tarefas.repository.TarefaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TarefaService {
+
+    private TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    // Adicionar Tarefa
+    public TarefaModel adicionarTarefa(TarefaModel tarefaModel) {
+        return tarefaRepository.save(tarefaModel);
+    }
+
+    // Listar Tarefas
+    public List<TarefaModel> listarTarefas() {
+        return tarefaRepository.findAll();
+    }
+
+    // Listar Tarefa Por ID
+    public TarefaModel listarTarefaPorID(Long id) {
+        return tarefaRepository.findById(id).orElse(null);
+    }
+
+    // Deletar Tarefa Por ID
+    public void deletarTarefaPorId(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+}
