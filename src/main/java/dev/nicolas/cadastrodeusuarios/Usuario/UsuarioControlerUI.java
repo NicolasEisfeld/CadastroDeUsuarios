b24c56c20a62dca9d8178460d7f022db9b07f79d
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2026-01-23 12:31:48 -0300

    feat: adiciona a camada de controle para view direto através do thymeleaf, com páginas de cadastro HTML

---
+++ src/main/java/dev/nicolas/cadastrodeusuarios/Usuario/UsuarioControlerUI.java
@@ -0,0 +1,34 @@
+package dev.nicolas.cadastrodeusuarios.Usuario;
+
+import dev.nicolas.cadastrodeusuarios.Usuario.dto.UsuarioDTO;
+import dev.nicolas.cadastrodeusuarios.Usuario.service.UsuarioService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UsuarioControlerUI {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioControlerUI(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/cadastro")
+    public String exibirFormularioCadastro(Model model) {
+        model.addAttribute("usuario", new UsuarioDTO());
+        return "cadastro/adicionarUsuario";
+    }
+
+    @PostMapping("/cadastro")
+    public String adicionarUsuario(@ModelAttribute("usuario") UsuarioDTO usuarioDTO) {
+        try {
+            usuarioService.adicionarUsuario(usuarioDTO);
+            return "cadastro/cadastroSucesso";
+        } catch (Exception e) {
+            return "cadastro/cadastroErro";
+        }
+    }
+}
