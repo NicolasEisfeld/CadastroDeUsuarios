dd2c49c248a7320370925565a83428b0acbe0c86
NicolasEisfeld <nicolaseisfeld.dev@gmail.com>
2026-01-28 10:10:32 -0300

    feat: adiciona as funções de alterar e deletar para a página de Tarefas

--- src/main/java/dev/nicolas/cadastrodeusuarios/Tarefas/TarefaControllerUI.java
+++ src/main/java/dev/nicolas/cadastrodeusuarios/Tarefas/TarefaControllerUI.java
@@ -4,7 +4,10 @@ import dev.nicolas.cadastrodeusuarios.Tarefas.dto.TarefaDTO;
 import dev.nicolas.cadastrodeusuarios.Tarefas.service.TarefaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -22,6 +25,19 @@ public class TarefaControllerUI {
         List<TarefaDTO> tarefas = tarefaService.listarTarefas();
         model.addAttribute("tarefas", tarefas);
         return "tarefas/listarTarefas";
+    }
+
+    @PutMapping
+    public String alterarTarefaPorId(@PathVariable long id, Model model) {
+        TarefaDTO tarefaDTO = tarefaService.listarTarefaPorID(id);
+        model.addAttribute("tarefa", tarefaDTO);
+        return "tarefas/alterarTarefa";
+    }
+
+    @DeleteMapping("/tarefas/deletar/{id}")
+    public String deletarTarefaPorId(@PathVariable long id) {
+        tarefaService.deletarTarefaPorId(id);
+        return "tarefas/deletarTarefaSucesso";
     }
 
 
